f4873eb8bb22e853c993b4feb183ac0fcbada216
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-03 22:43:53 -0400

    created connection.ts for database

---
+++ server/src/config/connection.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Sequelize } from 'sequelize';
+
+const sequelize = process.env.DB_URL
+    ? new Sequelize(process.env.DB_URL)
+    : new Sequelize(
+        process.env.DB_NAME || '',
+        process.env.DB_USER || '',
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'postgres',
+            schema: 'amygdalaDB',
+            dialectOptions: {
+                decimalNumbers: true,
+            },
+        }
+        );
+
+// Create schema if it doesn't exist
+sequelize.query('CREATE SCHEMA IF NOT EXISTS your_schema_name;')
+    .then(() => {
+        console.log('Schema created or already exists');
+    })
+    .catch(err => {
+        console.error('Error creating schema:', err);
+    });
+
+export default sequelize;
