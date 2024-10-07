ee8b1cb89c45a005d007dfd3c0548df5d3c60a1d
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 17:52:27 -0400

    created connection with sequelize models/db/express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import sequelize from './config/connection';
+
+const forceDatabaseRefresh = false;
 
 dotenv.config();
 
@@ -10,6 +13,9 @@ app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+// sync conntection to database
+sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+})
