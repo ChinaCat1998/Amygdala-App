d1aaa253827a8dedddde8440c3b37a499dcf91a0
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 21:44:17 -0400

    setup functions for user routes; WIP

---
+++ server/src/controllers/user-controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { User } from "../models/user";
+
+// GET /api/users - gets all users
+
+// GET /api/users/:id - gets a single user by id
+
+// POST /api/users - creates a new user
+
+// PUT /api/users/:id - updates a user by id
+
+// DELETE /api/users/:id - deletes a user by id
