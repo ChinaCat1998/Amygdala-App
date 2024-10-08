7cb877ea821b08c4bdf392ddb55d93dfa6173a28
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 21:43:21 -0400

    index.ts router for all routes; /api & /auth maybe

---
+++ server/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+// import api/maybe auth routes here
+// import apiRoutes from './api/index';
+
+const router = Router();
+
+// router.use('/auth', authRoutes); maybe...
+// router.use('/api', apiRoutes);
+
+export default router;
