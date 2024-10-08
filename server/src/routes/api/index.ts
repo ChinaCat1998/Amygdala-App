f1cd20f0a52a8087228e72fd2c163aaa8820c9ec
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 22:17:14 -0400

    completed userRouter

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-//imports for /api/routes here
+//imports for /api/routes here: user, journalEntry, maybe auth
+import { userRouter } from './user-routes';
+// import { journalEntryRouter } from './journalEntry-routes';
+// import { authRouter } from middleware
 
 const router = Router();
 
 // authenticate token for just journalEnry routes? maybe...
-// router.use('users', userRouter); 
+router.use('users', userRouter); 
 // router.use('journalEntries', journalEntryRouter);
 
 export default router;
