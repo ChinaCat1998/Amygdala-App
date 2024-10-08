29f92cfc9f8b39161eed05801849f914223e2349
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 22:12:58 -0400

    created functions for user routes

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -2,11 +2,88 @@ import { Request, Response } from "express";
 import { User } from "../models/user";
 
 // GET /api/users - gets all users
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.findAll({
+            attributes: { exclude: ['password'] }
+        });
+        res.json(users);
+    }
+    catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 // GET /api/users/:id - gets a single user by id
+export const getUserById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findByPk(id, {
+            attributes: { exclude: ['password'] }
+        });
+        if (user){
+            res.json(user);
+        }
+        else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
+    catch (error: any) {
+        res.status(500).json({ message: error.message });
+
+    }
+}
 
 // POST /api/users - creates a new user
+export const createUser = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+        const newUser = await User.create({ username, password});
+        res.status(201).json(newUser);
+    }
+    catch (error: any) {
+        res.status(400).json({ message: error.message });
+
+    }
+}
 
 // PUT /api/users/:id - updates a user by id
+export const updateUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { username, password } = req.body;
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            user.username = username;
+            user.password = password;
+            await user.save();
+            res.json(user);
+        }
+        else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
+    catch (error: any) {
+        res.status(400).json({ message: error.message });
+
+    }
+}
 
 // DELETE /api/users/:id - deletes a user by id
+export const deleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            await user.destroy();
+            res.json({ message: 'User deleted' });
+        }
+        else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
+    catch (error: any) {
+        res.status(500).json({ message: error.message });
+
+    }
+}
