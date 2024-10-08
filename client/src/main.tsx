10aec891c5da8a57b1393d152c5cd4e5bcf219c1
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-10-07 22:07:50 -0400

    fixed errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,10 +8,12 @@ import './index.css'
 // import ErrorPage from './pages/ErrorPage.tsx';
 // import LoginPage from './pages/LoginPage.tsx';
 // import SignUpPage from './pages/SignUpPage.tsx';
+// import Calandar from './pages/Calendar.tsx';
+// import HealthyTips from './pages/HealthyTips.tsx';
 
+//const App = () => {
 
-
-
+//}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
