f757bbf970544e598a3bc111969e1c79a56e438a
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 22:15:12 -0400

    linked user-controller functions to user routes

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -1,21 +1,23 @@
 import express from 'express';
-//import functions from controller file for routes here
+import {
+    getAllUsers, getUserById, createUser, updateUser, deleteUser,
+} from '../../controllers/user-controller';
 
 const userRouter = express.Router();
 
 // GET /api/users - gets all users
-// userRouter.get('/', getAllUsers);
+userRouter.get('/', getAllUsers);
 
 // GET /api/users/:id - gets a single user by id
-// userRouter.get('/:id', getSingleUser);
+userRouter.get('/:id', getUserById);
 
 // POST /api/users - creates a new user
-// userRouter.post('/', createUser);
+userRouter.post('/', createUser);
 
 // PUT /api/users/:id - updates a user by id
-// userRouter.put('/:id', updateUser);
+userRouter.put('/:id', updateUser);
 
 // DELETE /api/users/:id - deletes a user by id
-// userRouter.delete('/:id', deleteUser);
+userRouter.delete('/:id', deleteUser);
 
 export { userRouter };
