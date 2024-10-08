93c1bd81e28cdbcf81ff2464d5ea127bdb1ac057
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-10-07 21:10:17 -0400

    fixed bugs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// import Home from './pages/Home.tsx';
+// import JournalPage from './pages/JournalPage.tsx';
+// import ErrorPage from './pages/ErrorPage.tsx';
+// import LoginPage from './pages/LoginPage.tsx';
+// import SignUpPage from './pages/SignUpPage.tsx';
+
+
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
