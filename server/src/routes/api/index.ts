ee9f242aa32790c055ad0e1e27be6303d7cb5e05
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-07 21:46:30 -0400

    routes under /api; users and journalEntries; WIP

---
+++ server/src/routes/api/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+//imports for /api/routes here
+
+const router = Router();
+
+// authenticate token for just journalEnry routes? maybe...
+// router.use('users', userRouter); 
+// router.use('journalEntries', journalEntryRouter);
+
+export default router;
