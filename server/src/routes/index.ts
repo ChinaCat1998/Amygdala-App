a9d9a4978820a8d55a73f56885021502baf9b35b
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-10 19:46:08 -0400

    added auth routes to root index.ts routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-// import api/maybe auth routes here
+// import api/ auth routes here
 import apiRoutes from './api/index';
+import authRoutes from './auth-routes';
 
 const router = Router();
 
-// router.use('/auth', authRoutes); maybe...
 router.use('/api', apiRoutes);
+router.use('/auth', authRoutes);
 
 export default router;
