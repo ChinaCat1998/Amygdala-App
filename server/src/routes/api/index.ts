2af18dd8b28790cb37f9ea8968d10f6e62b36db2
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-10 19:43:22 -0400

    comment suggestions for future feature

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -8,6 +8,7 @@ const router = Router();
 
 router.use('/users', userRouter); 
 // authenticate token for just journalEnry routes? maybe...
+// router.use('/journal-entries', authenticateToken, journalEntryRouter);
 router.use('/journal-entries', journalEntryRouter);
 
 export default router;
