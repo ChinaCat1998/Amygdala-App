652c23220b9691a64d7f440c261dafb93d675113
sketchyTK <antkinney@gmail.com>
2024-10-13 16:13:01 -0500

    CSS updates, added comments to Compontents/Calendar

--- server/src/seeds/journal-seeds.ts
+++ server/src/seeds/journal-seeds.ts
@@ -1,34 +1,34 @@
-// import { JournalEntry } from '../models/index.js';
+import { JournalEntry } from '../models/index.js';
 
-// export const seedJournalEntries = async () => {
-//   await JournalEntry.bulkCreate([
-//     {
-//     title: 'Work is hard',
-//     userId: 2,
-//     mood: 'Frustrated',
-//     content: 'Had a difficult day at work today.',
-//     triggers: ['Work']
-//     },
-//     {
-//     title: 'School',
-//     userId: 2,
-//     mood: 'Frustrated',
-//     content: 'Had a difficult day at school today.',
-//     triggers: ['Work', 'School']
-//     },
-//     {
-//     title: 'Feeling Good!',
-//     userId: 1,
-//     mood: 'Happy',
-//     content: 'Hung out with friends and got food.',
-//     triggers: []
-//     },
-//     {
-//     title: 'We\'re so back',
-//     userId: 3,
-//     mood: 'Great',
-//     content: 'Gonna do 1000 push ups and watch Adam Sandler movies.',
-//     triggers: ['Life']
-//     },
-//   ]);
-// };
+export const seedJournalEntries = async () => {
+  await JournalEntry.bulkCreate([
+    {
+    date: '2024-10-10',
+    userId: 2,
+    mood: 'Frustrated',
+    content: 'Had a difficult day at work today.',
+    triggers: ['Work']
+    },
+    {
+    date: '2024-10-11',
+    userId: 2,
+    mood: 'Frustrated',
+    content: 'Had a difficult day at school today.',
+    triggers: ['Work', 'School']
+    },
+    {
+    date: '2024-10-12',
+    userId: 1,
+    mood: 'Happy',
+    content: 'Hung out with friends and got food.',
+    triggers: []
+    },
+    {
+    date: '2024-10-09',
+    userId: 3,
+    mood: 'Great',
+    content: 'Gonna do 1000 push ups and watch Adam Sandler movies.',
+    triggers: ['Life']
+    },
+  ]);
+};
