5f523b5d6c665a9ac075f52f676fc65840c4d93b
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-12 21:18:05 -0400

    commented out; needs to be altered to match date property

--- server/src/seeds/journal-seeds.ts
+++ server/src/seeds/journal-seeds.ts
@@ -1,34 +1,34 @@
-import { JournalEntry } from '../models/index.js';
+// import { JournalEntry } from '../models/index.js';
 
-export const seedJournalEntries = async () => {
-  await JournalEntry.bulkCreate([
-    {
-    title: 'Work is hard',
-    userId: 2,
-    mood: 'Frustrated',
-    content: 'Had a difficult day at work today.',
-    triggers: ['Work']
-    },
-    {
-    title: 'School',
-    userId: 2,
-    mood: 'Frustrated',
-    content: 'Had a difficult day at school today.',
-    triggers: ['Work', 'School']
-    },
-    {
-    title: 'Feeling Good!',
-    userId: 1,
-    mood: 'Happy',
-    content: 'Hung out with friends and got food.',
-    triggers: []
-    },
-    {
-    title: 'We\'re so back',
-    userId: 3,
-    mood: 'Great',
-    content: 'Gonna do 1000 push ups and watch Adam Sandler movies.',
-    triggers: ['Life']
-    },
-  ]);
-};
+// export const seedJournalEntries = async () => {
+//   await JournalEntry.bulkCreate([
+//     {
+//     title: 'Work is hard',
+//     userId: 2,
+//     mood: 'Frustrated',
+//     content: 'Had a difficult day at work today.',
+//     triggers: ['Work']
+//     },
+//     {
+//     title: 'School',
+//     userId: 2,
+//     mood: 'Frustrated',
+//     content: 'Had a difficult day at school today.',
+//     triggers: ['Work', 'School']
+//     },
+//     {
+//     title: 'Feeling Good!',
+//     userId: 1,
+//     mood: 'Happy',
+//     content: 'Hung out with friends and got food.',
+//     triggers: []
+//     },
+//     {
+//     title: 'We\'re so back',
+//     userId: 3,
+//     mood: 'Great',
+//     content: 'Gonna do 1000 push ups and watch Adam Sandler movies.',
+//     triggers: ['Life']
+//     },
+//   ]);
+// };
