d9560c8c463461aa36b1d14fc441da85689e2525
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-13 22:14:48 -0400

    edited route for getJournalEntryByDate

--- server/src/routes/api/journal-entry-routes.ts
+++ server/src/routes/api/journal-entry-routes.ts
@@ -12,8 +12,8 @@ journalEntryRouter.get('/all', getAllJournalEntriesAllUsers);
 // GET /api/journal-entries - gets all journal entries -- UNUSED BY CLIENT UI
 journalEntryRouter.get('/', getAllJournalEntries);
 
-// GET /api/journal-entries with date query parameter
-journalEntryRouter.get('/', getJournalEntryByDate);
+// GET /api/journal-entries/date with date query parameter
+journalEntryRouter.get('/date', getJournalEntryByDate);
 
 // GET /api/journal-entries/:id - gets a single journal-entry by id
 journalEntryRouter.get('/:id', getJournalEntryById);
