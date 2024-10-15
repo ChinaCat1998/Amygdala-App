34e452ead81860e90963966d2bf37200ed229987
sketchyTK <antkinney@gmail.com>
2024-10-14 21:40:56 -0500

    updating nav2 menu active state CSS

--- client/src/components/Nav2.tsx
+++ client/src/components/Nav2.tsx
@@ -6,15 +6,15 @@ const NavMenu = () => {
     <nav className='updated-nav-tk'>
       <ul>
         <li>
-          <Link to='/JournalPage' className={currentPage === '/JournalPage' ? 'nav-link active' : 'nav-link'}>Journal
+          <Link to='/JournalPage' className={currentPage === '/JournalPage' ? 'active' : 'nav-link'}>Journal
           </Link>
         </li>
         <li>
-          <Link to='/CalendarPage' className={currentPage === '/CalendarPage' ? 'nav-link active' : 'nav-link'}>Calendar
+          <Link to='/CalendarPage' className={currentPage === '/CalendarPage' ? 'active' : 'nav-link'}>Calendar
           </Link>
         </li>
         <li>
-          <Link to='/TipsPage' className={currentPage === '/TipsPage' ? 'nav-link active' : 'nav-link'}>Tips
+          <Link to='/TipsPage' className={currentPage === '/TipsPage' ? 'active' : 'nav-link'}>Tips
           </Link>
         </li>
       </ul>
