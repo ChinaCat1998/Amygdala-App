378294d2496cc107d4380e6fca3dc02407340b44
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-15 16:59:02 -0400

    added login check for home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,11 +3,35 @@ import Header from '../components/Header';
 import Footer from "../components/Footer";
 import Nav2 from "../components/Nav2"
 import Nav from "../components/Nav"
+import { useEffect, useState, useLayoutEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Auth from '../utils/auth';
 // import Nav from "../components/Nav";
 
 
 
 function HomePage () {
+    const [loginCheck, setLoginCheck] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkLogin = () => {
+            if (Auth.loggedIn()) {
+                setLoginCheck(true);  // User is logged in
+            } else {
+                // If not logged in, log out and redirect
+                Auth.logout();
+                alert(`Not Logged In/Session Expired!\nPlease Log In`);
+                navigate('/login');  // Redirect to login page
+            }
+        };
+        checkLogin();
+    }, [navigate]);
+
+    if (!loginCheck) {
+        return null;  // Render nothing until login check is done
+    }
+
     return (
         <div className="welcome-wrap">
             <Header />
