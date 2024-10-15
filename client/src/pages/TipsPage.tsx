40e5652a4239cbcb54f1e1e8565ca19982d46734
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-15 17:05:56 -0400

    added login check

--- client/src/pages/TipsPage.tsx
+++ client/src/pages/TipsPage.tsx
@@ -4,8 +4,32 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 // import Nav from '../components/Nav';
 import Nav2 from '../components/Nav2';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 const TipsPage = () => {
+  const [loginCheck, setLoginCheck] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkLogin = () => {
+            if (Auth.loggedIn()) {
+                setLoginCheck(true);  // User is logged in
+            } else {
+                // If not logged in, log out and redirect
+                Auth.logout();
+                alert(`Not Logged In/Session Expired!\nPlease Log In`);
+                navigate('/login');  // Redirect to login page
+            }
+        };
+        checkLogin();
+    }, [navigate]);
+
+    if (!loginCheck) {
+        return null;  // Render nothing until login check is done
+    }
+
   return (
     <div className="App">
       <Header />
