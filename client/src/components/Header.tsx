6b2c2ade5e38e05597e957d0974152a0bf6b9acb
Leo <lshapirov2@gmail.com>
2024-10-15 23:25:56 -0500

    logout button added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,22 @@
 /*  */
 import logo from '../assets/logo/amygdala_logo-crop.jpg'
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Auth from '../utils/auth';
+import Logout from "../assets/icons/logout.png"
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Auth.logout();
+    console.log('User logged out');
+    navigate('/LoginPage'); // Redirect to login page
+  };
   return (
     <header>
-      <a href="/Home"><img src={logo} alt="Amygdala - Healing Starts Here" /></a>
+      <button className="logoutButton" onClick={handleLogout}><img className='logoutPicture' src={Logout} alt="logout" /></button>
+      <a href="/Home"><img className='headerLogo' src={logo} alt="Amygdala - Healing Starts Here" /></a>
     </header>
   );
 };
